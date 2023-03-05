3e2b4a565e62fc2a2d9f9e0ee97a7377348064f4
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2023-03-05 14:48:54 -0300

    update

--- bin/projeto_dart_2_estudo_sintaxe.dart
+++ bin/projeto_dart_2_estudo_sintaxe.dart
@@ -22,9 +22,66 @@ void main() {
   print(funcEstaMadura(40));
 
   /* Há 4 tipos de parâmetros nas funções em Dart:
-  1 -- posicionais obrigatórios
-  2 -- Nomeados Opcionais
-  3 
+  1 -- Posicionais obrigatórios - A ordem de inclusão na função faz diferença
+  2 -- Nomeados Opcionais - Podem ou não ser incluídos na função
+  3 -- Parâmetros com "Padrão" - O parâmetro recebe um padrão
+  4 -- Modificador "required" - se coloca o modificador required para obrigar a passagen
+                                 do parâmetro que antes era opcional
   */
 
+  mostrarMadura(String fruta, int dias,
+      {required String cor, int quantidade = 10}) {
+    if (dias >= 30) {
+      print('A $fruta está madura');
+    } else {
+      print('A $fruta não está madura');
+    }
+    print(cor);
+    print(quantidade);
+  }
+
+  /* Não esquecer que Dart possui o null Safety. Assim, para aceitar parâmetros nulos deve-se usar
+   "String?", "int?" e assim por diante */
+  // O parâmetro opcional deve ser passado com seu nome
+
+  mostrarMadura('uva', 40, cor: 'vermelha');
+
+  // classes sempre começam com letra maiúscula
+
+  Fruta fruta01 = Fruta('morango', 100, 'Vermelho', 'Cítrico', 40);
+  print(fruta01.Nome);
+  print(fruta01);
+
+  fruta01.mostrarFruta();
+  print('-----');
+  fruta01.mostrarMadura();
+}
+
+class Fruta {
+  String Nome;
+  double peso;
+  String cor;
+  int diasDesdeColheita;
+  int? quantidade;
+  String sabor;
+  bool?
+      isMadura; // o interrogação é para dizer que a variável pode ser nula, senão o null safety age
+
+  Fruta(this.Nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita,
+      {this.isMadura}); // as informações sobre parâmetros se aplicam aqui também
+
+  mostrarMadura() {
+    if (this.diasDesdeColheita >= 30) {
+      print('Fruta Madura');
+    } else
+      print('Fruta não madura');
+  }
+
+  mostrarFruta() {
+    print("O Nome da fruta é " + this.Nome);
+    print("O peso  da fruta é $peso");
+    print("A cor da fruta é " + this.cor);
+    print("a dia foi colhida há $diasDesdeColheita dias");
+    print("O sabor da fruta é " + this.sabor);
+  }
 }
